195cdef66a58939b206fce0fe27075b526c3c4ae
Artur <arturwaquil@gmail.com>
2021-03-11 20:02:38 -0300

    gui ok

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import sys
+import math
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "True"
 import pygame
@@ -8,29 +9,41 @@ from game import Game2048
 class GUI:
 
     Colors = {
-        "light_gray": (205, 191, 179),
-        "medium_gray": (185, 169, 157),
-        "dark_gray": (119, 109, 101),
+        "gray_light": (205, 191, 179),
+        "gray_medium": (185, 169, 157),
+        "gray_dark": (119, 109, 101),
         "white": (237, 226, 217),
+        "whitish": (238, 225, 201),
+        "orange": (243, 178, 122),
+        "orange_dark": (246, 150, 100),
+        "redish": (247, 124, 95),
+        "red": (247, 95, 59),
+        "yellow_1": (237, 208, 115),
+        "yellow_2": (237, 204, 98),
+        "yellow_3": (237, 201, 80),
+        "yellow_4": (237, 197, 63),
+        "yellow_5": (237, 194, 46),
+        "black": (60, 58, 51)
     }
 
     def __init__(self, n):
         pygame.init()
 
+        # Info on dimensions
         self.n = n
-        
-        pygame.display.set_caption("2048")
-
-        self.tile_side = 100
+        self.tile_side = 130
         self.margin = 10
         self.score_board_height = 40
         self.width = self.tile_side*n + self.margin
         self.height = self.width + self.score_board_height
         self.size = (self.width, self.height)
         
+        # The game screen
+        pygame.display.set_caption("2048")
         self.screen = pygame.display.set_mode(self.size)
-        self.screen.fill(self.Colors["medium_gray"])
+        self.screen.fill(self.Colors["gray_medium"])
 
+        # Game core
         self.game = Game2048(self.n)
         self.game_status = Game2048.IN_GAME
 
@@ -61,37 +74,52 @@ class GUI:
 
         # Print score in the upper part of the window
         score_space = pygame.Surface((self.width, self.score_board_height))
-        score_space.fill(self.Colors["medium_gray"])
+        score_space.fill(self.Colors["gray_medium"])
         self.screen.blit(score_space, (0,0))
         font = pygame.font.Font(pygame.font.get_default_font(), self.score_board_height-self.margin)
-        label = font.render("SCORE: " + str(score), True, self.Colors["dark_gray"])
+        label = font.render("SCORE: " + str(score), True, self.Colors["white"])
         self.screen.blit(label, (self.margin,self.margin))
-        pygame.display.flip()
 
         # Paint all tiles
         for row in range(self.n):
             for col in range(self.n):
                 self.paint_tile(board[row, col], (row, col))
 
+        pygame.display.flip()
+
     def paint_tile(self, value, position):
         tile = pygame.Surface((self.tile_side-self.margin, self.tile_side-self.margin))
 
-        row_pos = self.margin + self.tile_side*position[0] + self.score_board_height
-        col_pos = self.margin + self.tile_side*position[1]
+        row_pos = self.tile_side*position[0] + self.margin + self.score_board_height
+        col_pos = self.tile_side*position[1] + self.margin
         screen_pos = (col_pos, row_pos)
 
         if value == 0:
-            tile.fill(self.Colors["light_gray"])
+            tile.fill(self.Colors["gray_light"])
             self.screen.blit(tile, screen_pos)
         else:
-            tile.fill(self.Colors["white"])
-            font = pygame.font.Font(pygame.font.get_default_font(), 50)
-            label = font.render(str(value), True, self.Colors["dark_gray"])
+            tile.fill(self.Colors[self.get_tile_color(value)])
             self.screen.blit(tile, screen_pos)
-            self.screen.blit(label, screen_pos)
+            font = pygame.font.Font(pygame.font.get_default_font(), self.get_label_font_size(value))
+            label = font.render(str(value), True, self.Colors[self.get_label_color(value)])
+            half_tile_side = (self.tile_side-self.margin)/2
+            label_rect = label.get_rect(center=(col_pos+half_tile_side, row_pos+half_tile_side))
+            self.screen.blit(label, label_rect)
         
-        pygame.display.flip()
 
+    def get_tile_color(self, value):
+        colors = ["white", "whitish", "orange", "orange_dark", "redish", "red", 
+                  "yellow_1", "yellow_2", "yellow_3", "yellow_4", "yellow_5", "black"]
+        return colors[min(int(math.log(value,2))-1, 11)]
+    
+    def get_label_color(self, value):
+        return "gray_dark" if value in [2,4] else "white"
+    
+    def get_label_font_size(self, value):
+        if value <= 64: return 60
+        elif value <= 512: return 50
+        elif value <= 2048: return 40
+        else: return 30
 
 if __name__ == "__main__":
     GUI(4).run()
